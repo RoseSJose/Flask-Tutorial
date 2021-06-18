df4eef78c6d1d07d0ff6c8bd532b891fa6680287
Rose S Jose <rosesjose29@gmail.com>
2021-06-18 13:03:16 +0530

    url parsing

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)  #turn the current file to application
 @app.route("/")        #gives the route of the application
                        #@ is a python decorator
 def index():
-    return "Hello, World!"
+    return render_template("index.html", name=request.args.get("name"))
+        #flask parses the url and retrun the name
     
