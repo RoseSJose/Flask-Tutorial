6a382ee5143488429527651f822842a45c686705
Rose S Jose <rosesjose29@gmail.com>
2021-06-18 13:28:23 +0530

    Adding Forms

--- application.py
+++ application.py
@@ -4,6 +4,10 @@ app = Flask(__name__)  #turn the current file to application
 @app.route("/")        #gives the route of the application
                        #@ is a python decorator
 def index():
-    return render_template("index.html", name=request.args.get("name"))
-        #flask parses the url and retrun the name
-    
+    return render_template("index.html")
+        #flask parses the url(?name=David) and return the name
+        #2nd argument to get is the default argument
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name","world"))
